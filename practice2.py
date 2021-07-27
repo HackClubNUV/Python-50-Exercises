4a8cd8a0893edff768772eab3297f6be7a25e696
Samarth Shah <57283918+samarth-shah@users.noreply.github.com>
2021-07-27 18:47:49 +0530

    Update practice2.py

--- practice2.py
+++ practice2.py
@@ -1,9 +1,16 @@
-def mysum(*num):
+#EXERCISE 2 - Summing numbers
+
+def mysum(num):
     t = 0
     for num in num:
         t += num
     return t
     
-print(mysum(1,2,3))
-print(mysum(10,20,30,40))
 
+n = int(input('how many number you want to add'))
+list1 = []
+for i in range(0,n):
+    a = int(input())
+    list1.insert(i,a)
+
+print(mysum(list1))
