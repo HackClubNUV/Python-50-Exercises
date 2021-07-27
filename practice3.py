bb09d7f9417c9b632d36ae8d3ac6601d44226209
Samarth Shah <57283918+samarth-shah@users.noreply.github.com>
2021-07-27 17:51:29 +0530

    Add files via upload

---
+++ practice3.py
@@ -0,0 +1,15 @@
+def run_timing():
+    totalRuns = 0
+    totalTime = 0
+    while True:     
+        timing = input('Enter 10 km run time: ').strip()
+        
+        if not timing:   
+            break
+        
+        totalTime += float(timing)
+        totalRuns += 1
+    averageTime = totalTime / totalRuns
+    print(f'Average of {averageTime}, over {totalRuns} runs')
+    
+run_timing()
